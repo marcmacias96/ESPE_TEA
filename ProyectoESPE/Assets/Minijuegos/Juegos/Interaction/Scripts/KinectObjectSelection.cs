5fd91b656b1552919dbce0af78796cf3b02e8dee
Marco Macias <mamacias3@espe.edu.ec>
2019-06-19 19:29:58 -0500

    comit

---
+++ ProyectoESPE/Assets/Minijuegos/Juegos/Interaction/Scripts/KinectObjectSelection.cs
@@ -0,0 +1,74 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class KinectObjectSelection : MonoBehaviour {
+
+    //Es el objeto KinectController
+	public GameObject kinectcontrol;
+    //public Text texto;
+    //El nombre del objeto seleccionado actualmente
+    string activeobjname="";
+    //El Script del Kinect para Seleccionar Objetos
+	GrabDropScript sc;
+    //El string que se envia desde otro script para comprobar que sea el seleccionado, puede ser un color o nombre cualquiera, pero debe hacer referencia al nombre del objeto que se desea seleccionar
+    private static string color;
+    //Es para saber si el nombre del objeto seleccionado es el mismo del que se envio desde otro script. Comparacion hecha con variable color
+    private static bool isColorSelected = false;
+    
+
+
+	// Use this for initialization
+	void Start () {
+		sc=kinectcontrol.GetComponent<GrabDropScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+        //Toma el nombre del objeto seleccionado desde GrabDropScript a activeobjname
+		activeobjname=sc.objname;
+        Debug.Log("objselected: " + activeobjname + "  toselecte: " + color);
+        //Compara el nombre del objeto seleccionado con el nombre enviado desde otro script
+       if(activeobjname.Equals(color))
+        {
+            //Se indica que el nommbre del objeto seleccionado coincide con el nombre enviado desde otro script
+            isColorSelected = true;
+            //Se limpia el nombre del objeto asignado
+            activeobjname = "";
+        }
+        else
+        {
+            //Se indica que el nommbre del objeto seleccionado no coincide con el nombre enviado desde otro script
+            isColorSelected = false;
+        }
+       
+       
+	}
+
+
+    //Setter de color
+    public static void  setColor(string setColor)
+    {
+        color = setColor;
+    }
+
+    //Getter de color
+    public static string getColor()
+    {
+        return color;
+    }
+
+    //Setter de iscolorselected
+    public static bool getisColorSelected()
+    {
+        return isColorSelected;
+    }
+
+    //Getter de iscolorselected
+    public static void setisColorSelected(bool sel)
+    {
+        isColorSelected = sel;
+    }
+}
--- ProyectoESPE/Assets/assets/Organizado/Minijuegos/Juego 1 Flores/Scripts/GameManagerFlores.cs
+++ ProyectoESPE/Assets/assets/Organizado/Minijuegos/Juego 1 Flores/Scripts/GameManagerFlores.cs
@@ -5,107 +5,158 @@ using UnityEngine.UI;
 
 public class GameManagerFlores : MonoBehaviour
 {
-
+    //Es el canvas donde se muestra el tutorial de inicio
     public GameObject canvTutorial;
+    //Es el canvas donde se muestra la flor que debe ser elegida
     public GameObject canvPistal;
+    //Es el canvas donde se muestra si la seleccion fue correcta
     public GameObject canvCorrecto;
+    //Es el puntero de seleccion (manito)
     public GameObject gesturePointer;
+    //El tiempo de espera entre cada estado asignado desde el editor
     public float time;
 
+
     private static float waitTime;
-    private bool doTutorial;
-    private bool isPlaying;
-    IEnumerator co;
-    IEnumerator IsInactive;
+    //private bool doTutorial;
+    //private bool isPlaying;
+    //Lista de colores a elegir
     private List<string> colorsList = new List<string>();
-    public Sprite[] images;
+    //Lista de imagenes de flores
+    public List<Sprite> images;
+
     private int counter = 5;
+    //Objeto que muestra la imagen de la flor que se debe seleccionar
     public GameObject showImage;
 
+    //1.-
     void Start()
     {
+        //Anadir colores a la lista colorsList
         colorsList.Add("yellow");
         colorsList.Add("green");
         colorsList.Add("blue");
         colorsList.Add("red");
         //gesturePointer.SetActive(false);
-        doTutorial = false;
-        isPlaying = false;
-        setWaitTime(time);
+        //doTutorial = false;
+        //isPlaying = false;
+        //Inicializar el tiempo con la variable time
+        //setWaitTime(time);
         //co = showAndDisable(canvTutorial);
+        //Inicia tutorialtime y le envia el canvas del tutorial
         StartCoroutine(tutorialTime(canvTutorial));
        
     }
 
+    //3.1.-
     void ChangeImage(int value)
     {
+        //Se elije el sprite dentro de la lista de imagenes de flores
         Sprite im = images[value];
+        Debug.Log("value: " + value);
+        //Se asigna el sprite que se debe mostrar en la variable showImage, que es el objeto dentro del canvas pista1
         showImage.GetComponent<Image>().sprite = im;
 
     }
 
-    IEnumerator waitToSelect()
+    //6.-
+    IEnumerator waitToSelect(int selected)
     {
+        //Espera por 1 segundo
         yield return new WaitForSeconds(1);
+        //Le resta uno al contador counter que esta en 5 despues de cada instruccion
         counter--;
+        //Chequea si el contador es 0, o sea si han si han pasado 5 segundos
+        Debug.Log("Contador: " + counter);
         if (counter == 0)
         {
             //gesturePointer.SetActive(false);
-            StartCoroutine(showAndDisable(canvPistal));
+            //Llama a showAndDisable y le manda el canvas pista1 que es donde se muestran las instrucciones
+            //StartCoroutine(showAndDisable(canvPistal));
+            counter = 5;
+            Choose(selected);
         }
         else
         {
-            Choose();
+            //Si no se han cumplido los 5 segundos llama a Choose nuevamente
+            Choose(selected);
         }
 
     }
 
+    //3.-
     IEnumerator showAndDisable(GameObject canvas)
     {
-        Debug.Log("reboot");
-        int selected = Random.Range(0, 4);
-        AnimalSelected.setColor(colorsList[selected]);
+        gesturePointer.SetActive(false);
+        //Selecciona al azar  un numero del 0 al tamano de la lista de colores
+        int selected = Random.Range(0, colorsList.Count);
+        Debug.Log("count: " + colorsList.Count);
+        Debug.Log("selected: " + selected);
+        //Setea en el KinecIbjectSelection del objeto KinectController, el color que debe seleccionarse
+        KinectObjectSelection.setColor(colorsList[selected]);
+        //Se envia a change image el indice del array de colorsList(5)
         ChangeImage(selected);
-        Debug.Log(AnimalSelected.getColor());
+        //Activa el canvas pista1
         canvas.SetActive(true);
+        //Espera el tiempo establecido
         yield return new WaitForSeconds(time);
+        //Desactiva el canvas
         canvas.SetActive(false);
-    
+        //Igual el contador a 5 para esperar los 5 segundos
         counter = 5;
-        Choose();
+        //Llama a Choose(5)
+        Choose(selected);
 
     }
 
-    public void Choose()
+    //5.-
+    public void Choose(int selected)
     {
-        //gesturePointer.SetActive(true);
-        Debug.Log(AnimalSelected.getisColorSelected());
-        if (AnimalSelected.getisColorSelected())
+        gesturePointer.SetActive(true);
+        Debug.Log("Seleccionado? "+KinectObjectSelection.getisColorSelected());
+        //Chequea si el color seleccionado por el jugador es el mismo que se asigno en el juego
+        if (KinectObjectSelection.getisColorSelected())
         {
             //gesturePointer.SetActive(false);
-            AnimalSelected.setisColorSelected(false);
-            Debug.Log("Dentro del if: "+AnimalSelected.getisColorSelected());
+            //setea la variable isColorSelected a false para que vuelva a iniciar el juego
+            KinectObjectSelection.setisColorSelected(false);
+            Debug.Log("Dentro del if: "+ KinectObjectSelection.getisColorSelected());
+            //Elimina el color elegido correctamente, de la lista de colores
+            colorsList.RemoveAt(selected);
+            images.RemoveAt(selected);
+            //Inicia tutorialTime con el canvas de haber elegido correctamente el color
             StartCoroutine(tutorialTime(canvCorrecto));
         }
         else
         {
-            StartCoroutine(waitToSelect());
+            //Se llama a waitToSelecte si el color correcto no ha sido seleccionado
+            StartCoroutine(waitToSelect(selected));
         }
     }
 
+    //2.-
     IEnumerator tutorialTime(GameObject canvas)
     {
+        //Pone el puntero en desactivado para que no se pueda seleccionar nada
+        gesturePointer.SetActive(false);
+        //Activa el canvas
         canvas.SetActive(true);
+        //Espera el tiempo establecido
         yield return new WaitForSeconds(time);
+        //Desactiva el canvas
         canvas.SetActive(false);
-        StartCoroutine(showAndDisable(canvPistal));
-    }
+        //Chequea si la lista de colores esta vacia para cambiar al estado de TIEMPOS.
+        if (colorsList.Count == 0)
+        {
+            //CAmbiar al estado TIEMPOS
 
-   
-
-    public  static void setWaitTime(float timeWait)
-    {
-        waitTime = timeWait;
+        }
+        else
+        {
+            //Inicia showAndDisable y le envia el canvas pista1 donde estan las imagenes de las flores a seleccionar
+            StartCoroutine(showAndDisable(canvPistal));
+        }
+        
     }
 }
     
